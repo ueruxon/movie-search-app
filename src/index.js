b68f51f3de8a087873735be2daf3c11567673fd8
Dima Matthew <thegobender@gmail.com>
2018-04-17 17:47:10 +0300

    локалСторейдж

--- src/index.js
+++ src/index.js
@@ -5,18 +5,42 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import promise from "redux-promise";
+import  _  from "lodash";
 
+import * as ls from './localStorage';
 import reducers from './reducers';
 
 import App from "./app";
 
-const store = createStore(reducers, applyMiddleware(promise));
+
+// --------- Сраный костыль ------
+
+const favoritesList = ls.load('favorites');
+
+const initialState = createStore(reducers);
+const prevStore = initialState.getState();
+
+const store = createStore(reducers, {
+    ...prevStore,
+    allMovies: {
+        ...prevStore.allMovies,
+        favorites: favoritesList || prevStore.allMovies.favorites
+    }
+}, applyMiddleware(promise));
+
+store.subscribe(_.debounce(() => {
+    const { allMovies: { favorites } } = store.getState();
+    ls.save('favorites', favorites);
+}), 200);
+
+// --------- Конец сраного костыля ------
+
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider >
-    , document.getElementById('root'));
+, document.getElementById('root'));
 
 
 
